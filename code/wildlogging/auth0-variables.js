f310c5b22cd9570598db91e6875da7a6cc6ff982
Coops <coopsatwork@googlemail.com>
2019-02-07 16:38:41 +0000

    removed wild logging draft code. It's now in the CMDT organisation

--- code/wildlogging/auth0-variables.js
+++
@@ -1,9 +0,0 @@
-window.auth_config = {
-  
-  "AUTH0_CLIENT_ID":"u9cmb34TNxuBCR4NT3vqQGolWGYIYGs6",
-  "AUTH0_DOMAIN":"urbanwild.eu.auth0.com",
-  "AUTH0_AUDIENCE": "https://www.urbanwild/wildloggingadmin",
-  "AUTH0_REDIRECT_URI" : "https://cmdt.github.io/SPWA_test/test.txt#hello",
-  "AUTH0_REQUESTED_SCOPES" : "openid profile email admin",
-  "AUTH0_RESPONSE_TYPE" : "token id_token"
-};
